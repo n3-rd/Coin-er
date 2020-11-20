eec800c45b71496f1b05dda7c7b9ecdd37949034
n3-rd <godwinjdayo@gmail.com>
2020-11-20 20:42:16 +0100

    added negative and positive coin change color

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,22 @@ var app = new Vue({
   data: {
     coinsData: [],
     dialog: false,
+    loaded: false,
+    changeV: '',
+    history: [
+  
+    ]
   },
   methods: {
     getCoinsData() {
       fetch("https://api.coinranking.com/v1/public/coins")
         .then((response) => response.json())
         .then((json) => (this.coinsData = json.data.coins));
+        // this.history = this.coinsData.map(i => {
+        //   if(i.history) return i.history;
+        //   return 0;
+        // });
+        this.loaded = true;
     },
     refresh(done) {
       setTimeout(() => {
@@ -16,8 +26,17 @@ var app = new Vue({
         done();
       }, 1000);
     },
+    changeColor: function(ch){
+      this.changeV =  ch
+    }
   },
   created() {
     this.getCoinsData();
+    window.fitText( document.querySelectorAll('.coin-website'), 10 );
   },
+  computed: {
+    chartSrc() {
+      return `https://quickchart.io/chart?c={type:'sparkline',data:{datasets:[{data:[${this.history}]}]}}`
+    }
+  }
 });
